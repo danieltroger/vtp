bb3eeb4f1edc04030b3748f1f40139747b3b47b7
daniel <daniel@Daniels-Mini-2.fritz.box>
2018-03-05 21:35:57 +0100

    version 1.8

--- vertretungsplan.php
+++ vertretungsplan.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Vertretungsplan
- * Version: 1.7
+ * Version: 1.8
  * Description: Just an empty plugin to install the vertretungsplan
  * Author: Daniel Troger
  * License: GPLv3
